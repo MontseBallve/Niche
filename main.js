48a9f24d47495aac8e8c1159fcf9ef8bd70e9ae6
MontseBallve <ballvemontse@gmail.com>
2022-12-05 16:37:14 -0300

    Rompiendo el sitio con Fetch

--- main.js
+++ main.js
@@ -154,3 +154,23 @@ const calcularTotal = () => {
     })
     total.innerHTML = `Total: $${totalCompra}`;
 }
+
+
+//Ruta relativa
+
+const listado = document.getElementById ("listado");
+const listadoProductos = "jason/productos.json";
+
+fetch(listadoProductos)
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        datos.forEach (producto => {
+            listado.innerHTML += `
+                <h2>Nombre: ${producto.nombre} </h2>
+                <p> Precio: ${producto.precio}</p>
+                <p> ID: ${producto.id}</p>
+            `
+        })
+    })
+    .catch(error => console.log(error))
+    .finally (() => console.log ("Proceso Finalizado"))
